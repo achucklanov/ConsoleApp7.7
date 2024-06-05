0a617691cf695e075627fba70b72db973cf87bec
AndreyChuck <a.chucklanov@yandex.ru>
2024-06-05 22:28:49 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp7._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуй, Мир!");
+        }
+    }
+}
